2ed3bccb15bfcc0537e261ecced4bcbc13fa0ffc
riabov-vladimir <vriabovsvet@gmail.com>
2022-06-04 20:01:02 +0300

    login works maybe

--- budgetreport/views.py
+++ budgetreport/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+from django.contrib import auth
+
+def auth_view(request):
+
+    # here you get the post request username and password
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+
+    # authentication of the user, to check if it active or None
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        if user.is_active:
+            # this is where the user login actually happens, before this the user
+            # is not logged in.
+            auth.login(request, user)
+
+            return ...
+
+    else :
+        return HttpResponseRedirect("Invalid username or password")
--- riabowcom/urls.py
+++ riabowcom/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.template.defaulttags import url
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('game/', include('game.urls'))
+    path('game/', include('game.urls')),
+    # url(r'^accounts/auth/$', 'auth_view'),
+    path('', include('django.contrib.auth.urls'))
 ]
