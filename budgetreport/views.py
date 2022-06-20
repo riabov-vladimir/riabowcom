c39bcf51c0c4d3728d25352b0a54d7963d21156f
wriabow <damnmeandyou@gmail.com>
2022-06-20 09:15:56 +0300

    css table moved to base.html

--- budgetreport/views.py
+++ budgetreport/views.py
@@ -10,109 +10,108 @@ from django.http import HttpResponseRedirect
 #   GET (видимо придется делать функцию либо добавлять в контекст новую инфу)
 
 def budget_summary(request):
-	employees = Employee.objects.all().order_by('position')
-	positions = EmployeePosition.objects.all().order_by('position')
+    employees = Employee.objects.all().order_by('position')
+    positions = EmployeePosition.objects.all().order_by('position')
 
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		form = AdditionalExpensesForm(request.POST)
+        form = AdditionalExpensesForm(request.POST)
 
-		if form.is_valid():
-			additional_expenses_item = AdditionalExpenses(expense_purpose=form.cleaned_data['expense_purpose'],
-			                                              expense_value=form.cleaned_data['expense_value'])
-			additional_expenses_item.save()
-			return HttpResponseRedirect('/budgetreport/summary/')
+        if form.is_valid():
+            additional_expenses_item = AdditionalExpenses(expense_purpose=form.cleaned_data['expense_purpose'],
+                                                          expense_value=form.cleaned_data['expense_value'])
+            additional_expenses_item.save()
+            return HttpResponseRedirect('/budgetreport/summary/')
 
-	# if a GET (or any other method) we'll create a blank form
-	else:
-		form = AdditionalExpensesForm()
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = AdditionalExpensesForm()
 
-	context = {
-		'employees': employees,
-		'positions': positions,
-		'form': form
-	}
+    context = {
+        'employees': employees,
+        'positions': positions,
+        'form': form
+    }
 
-	return render(request,
-	              'budgetreport/budget_summary.html',
-	              context
-	              )
+    return render(request,
+                  'budgetreport/budget_summary.html',
+                  context
+                  )
 
 
 # 2. Вкладка "Списанные часы", в экселе вкладка называлась по имени месяца отчёта
 #   GET, POST
 def employee_hours_view(request):
-	employee_hours_list = EmployeeHours.objects.all().order_by('-date_added')
+    employee_hours_list = EmployeeHours.objects.all().order_by('-date_added')
 
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		form = EmployeeHoursForm(request.POST)
+        form = EmployeeHoursForm(request.POST)
 
-		if form.is_valid():
-			employee_hours_item = EmployeeHours(
-				employee=form.cleaned_data['employee'],
-				hours=form.cleaned_data['hours'],
-				overtime=form.cleaned_data['overtime'],
-				task_description=form.cleaned_data['task_description'],
-				task_number=form.cleaned_data['task_number'],
-			)
-			employee_hours_item.save()
-			return HttpResponseRedirect('/budgetreport/summary/')
+        if form.is_valid():
+            employee_hours_item = EmployeeHours(
+                employee=form.cleaned_data['employee'],
+                hours=form.cleaned_data['hours'],
+                overtime=form.cleaned_data['overtime'],
+                task_description=form.cleaned_data['task_description'],
+                task_number=form.cleaned_data['task_number'],
+            )
+            employee_hours_item.save()
+            return HttpResponseRedirect('/budgetreport/summary/')
 
-	# if a GET (or any other method) we'll create a blank form
-	else:
-		form = EmployeeHoursForm()
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = EmployeeHoursForm()
 
-	context = {
-		'employee_hours_list': employee_hours_list,
-		'form': form
-	}
+    context = {
+        'employee_hours_list': employee_hours_list,
+        'form': form
+    }
 
-	return render(request,
-	              'budgetreport/employee_hours.html',
-	              context
-	              )
+    return render(request,
+                  'budgetreport/employee_hours.html',
+                  context
+                  )
 
 
 # 3. Вкладка "Работа по должностям"
 #   GET, POST
 def employee_position_hours_view(request):
-	employees = EmployeeHours.objects.all().order_by('-date_added')
-	additional_expenses = AdditionalExpenses.objects.all().order_by('-expense_value')
+    employees = EmployeeHours.objects.all().order_by('-date_added')
+    additional_expenses = AdditionalExpenses.objects.all().order_by('-expense_value')
 
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		form = AdditionalExpensesForm(request.POST)
+        form = AdditionalExpensesForm(request.POST)
 
-		if form.is_valid():
-			additional_expenses_item = AdditionalExpenses(expense_purpose=form.cleaned_data['expense_purpose'],
-			                                              expense_value=form.cleaned_data['expense_value'])
-			additional_expenses_item.save()
-			return HttpResponseRedirect('/budgetreport/employeepositionhours/')
+        if form.is_valid():
+            additional_expenses_item = AdditionalExpenses(expense_purpose=form.cleaned_data['expense_purpose'],
+                                                          expense_value=form.cleaned_data['expense_value'])
+            additional_expenses_item.save()
+            return HttpResponseRedirect('/budgetreport/employeepositionhours/')
 
-	# if a GET (or any other method) we'll create a blank form
-	else:
-		form = AdditionalExpensesForm()
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = AdditionalExpensesForm()
 
-	context = {
-		'additional_expenses': additional_expenses,
-		'form': form
-	}
+    context = {
+        'additional_expenses': additional_expenses,
+        'form': form
+    }
 
-	return render(request,
-	              'budgetreport/employee_position_hours.html',
-	              context
-	              )
+    return render(request,
+                  'budgetreport/employee_position_hours.html',
+                  context
+                  )
 
 # 4. Вкладка "Ставки"
 #   GET - Готово
-# TODO Отрисовать таблицу в шаблоне, наполнить данными
 
 
 class EmployeePositionList(ListView):
-	model = EmployeePosition
-	extra_context = {
-		'employees': Employee.objects.all().order_by('position')
-	}
+    model = EmployeePosition
+    extra_context = {
+        'employees': Employee.objects.all().order_by('position')
+    }
 
 
